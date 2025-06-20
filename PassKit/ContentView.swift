c3b7049575ceb77441475844880eacf0a4a18f97
wfxey <devinoldenburg16@gmail.com>
2025-06-20 22:27:11 +0200

    Transfered App )

--- PassKit/ContentView.swift
+++ PassKit/ContentView.swift
@@ -153,32 +153,6 @@ enum UtilityAction: String, CaseIterable, Identifiable {
     }
 }
 
-// MARK: - Main App
-
-@main
-struct PassKitApp: App {
-    @StateObject private var model = PassKitAppModel()
-    
-    var body: some Scene {
-        WindowGroup {
-            ZStack {
-                FrostBackground()
-                if model.showWelcome {
-                    WelcomeScreen {
-                        withAnimation { model.showWelcome = false }
-                    }
-                } else {
-                    AppContentView()
-                        .environmentObject(model)
-                }
-            }
-            .frame(minWidth: 880, minHeight: 600)
-        }
-        .windowStyle(.titleBar)
-        .windowResizability(.contentSize)
-    }
-}
-
 // MARK: - Glassmorphism Frosted Background
 
 struct FrostBackground: View {
--- PassKit/PassKitApp.swift
+++ PassKit/PassKitApp.swift
@@ -1,1 +1,26 @@
+import SwiftUI
+
+@main
+struct PassKitApp: App {
+    @StateObject private var model = PassKitAppModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            ZStack {
+                FrostBackground()
+                if model.showWelcome {
+                    WelcomeScreen {
+                        withAnimation { model.showWelcome = false }
+                    }
+                } else {
+                    AppContentView()
+                        .environmentObject(model)
+                }
+            }
+            .frame(minWidth: 880, minHeight: 600)
+        }
+        .windowStyle(.titleBar)
+        .windowResizability(.contentSize)
+    }
+}
 

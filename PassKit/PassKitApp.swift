3977f54d95b978b4433d061be8e16e6915c17edd
wfxey <devinoldenburg16@gmail.com>
2025-06-20 17:46:20 +0200

    Initial Commit

---
+++ PassKit/PassKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassKitApp.swift
+//  PassKit
+//
+//  Created by Devin Oldenburg on 20.06.25.
+//
+
+import SwiftUI
+
+@main
+struct PassKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PassKitTests/PassKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  PassKitTests.swift
+//  PassKitTests
+//
+//  Created by Devin Oldenburg on 20.06.25.
+//
+
+import Testing
+@testable import PassKit
+
+struct PassKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
